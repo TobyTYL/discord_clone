b1e69b5bee1f03638a4fe618e11d0ffb459e6ff2
TobyTYL <tobytaott@gmail.com>
2023-12-27 11:57:58 -0500

    update

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,9 +1,14 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div>
-      This is a protected route.
+      <UserButton
+        afterSignOutUrl='/'
+      />
+      <ModeToggle />
     </div>
   )
 }

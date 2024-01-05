2facd6129c488bfa4cdc91069f3ca1173d4b89b0
TobyTYL <tobytaott@gmail.com>
2024-01-05 12:16:10 -0500

    create the channel

--- discord-clone/components/providers/modal-provider.tsx
+++ discord-clone/components/providers/modal-provider.tsx
@@ -5,8 +5,8 @@ import { useEffect, useState } from "react";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
-import { MembersModal } from "../modals/members-modal";
-
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -25,6 +25,7 @@ export const ModalProvider = () => {
             <InviteModal />
             <EditServerModal />
             <MembersModal />
+            <CreateChannelModal />
         </>
     )
 }

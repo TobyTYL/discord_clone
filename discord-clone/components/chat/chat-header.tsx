cf7be0d2b9281a0046dbb6beaed21303951ff965
TobyTYL <tobytaott@gmail.com>
2024-01-08 22:26:08 -0500

    chat header prisma schema completion

--- discord-clone/components/chat/chat-header.tsx
+++ discord-clone/components/chat/chat-header.tsx
@@ -1,6 +1,4 @@
-import { Hash } from "lucide-react";
-
-import { UserAvatar } from "@/components/user-avatar";
+import { Hash, Menu } from "lucide-react";
 
 
 interface ChatHeaderProps {
@@ -18,21 +16,15 @@ export const ChatHeader = ({
 }: ChatHeaderProps) => {
     return (
         <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+            <Menu />
             {type === "channel" && (
                 <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
             )}
-            {type === "conversation" && (
-                <UserAvatar
-                    src={imageUrl}
-                    className="h-8 w-8 md:h-8 md:w-8 mr-2"
-                />
-            )}
+
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
             </p>
-            <div className="ml-auto flex items-center">
 
-            </div>
         </div>
     )
 }
--- discord-clone/components/mobile-toggle.tsx
+++ discord-clone/components/mobile-toggle.tsx
@@ -1,6 +1,10 @@
 import { Menu } from "lucide-react"
 
-
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+} from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 import { ServerSidebar } from "@/components/server/server-sidebar";
@@ -11,10 +15,18 @@ export const MobileToggle = ({
     serverId: string;
 }) => {
     return (
-
-        <div className="w-[72px]">
-            <NavigationSidebar />
-        </div>
-
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0 flex gap-0">
+                <div className="w-[72px]">
+                    <NavigationSidebar />
+                </div>
+                <ServerSidebar serverId={serverId} />
+            </SheetContent>
+        </Sheet>
     )
 }
